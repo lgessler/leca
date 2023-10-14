826b218bea8d2f2c85b23e3e8b7cbde909d0b21d
Luke Gessler <lukegessler@gmail.com>
2023-10-14 14:12:13 -0600

    remove two scripts

--- scripts/leca_poc.sh
+++
@@ -1,4 +0,0 @@
-python dump_dict.py
-bash scripts/prepare-wmt14en2de-mbart.sh
-python train.py -a transformer_wmt_en_de wmt14_en_de_toy_processed --max-sentences 1 --lr 0.00005 --max-epoch 50 --no-epoch-checkpoints
-bash scripts/run2.sh
